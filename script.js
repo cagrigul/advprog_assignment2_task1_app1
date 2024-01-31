698d6a72cebf5cb5bada9ce32421f20cc19f7c87
cagrigul <123596574+cagrigul@users.noreply.github.com>
2024-01-31 15:17:59 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+require(["esri/views/MapView", "esri/WebMap"], function(MapView, WebMap) {
+    var webmap = new WebMap({
+        portalItem: {
+            id: "5576d4d9c7e94a008699636f7c1d37cf" // Your webmap ID
+        }
+    });
 
+    webmap.load().then(function() {
+        // Change the basemap of the webmap after it has loaded
+        webmap.basemap = "dark-gray";
+
+        var view = new MapView({
+            map: webmap,
+            container: "viewDiv",
+            zoom: 6
+        });
+    });
+});
